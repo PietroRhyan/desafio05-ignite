0b75bf8828cab7c4c5ab366a770860b2d4f358b1
Pietro Rhyan <pietroqjg@gmail.com>
2022-04-27 15:44:22 -0300

    Pre-configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,9 +24,11 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <h1>Olá</h1>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
