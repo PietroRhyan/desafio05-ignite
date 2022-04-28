0e63fa948374af1783daef2720825e6025d9be8d
Pietro Rhyan <pietroqjg@gmail.com>
2022-04-27 22:37:48 -0300

    Generating static pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
+import styles from './header.module.scss'
+
+import Link from 'next/link' 
+
 export default function Header() {
-  // TODO
+  return (
+    <div className={styles.container}>
+      <Link href='/'>
+        <img src='/images/logo.svg' alt="logo" />
+      </Link>
+    </div>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
+import { FiCalendar, FiUser } from 'react-icons/fi'
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -26,13 +27,52 @@ interface HomeProps {
 
 export default function Home() {
   return (
-    <h1>Olá</h1>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h1>Como utilizar Hooks</h1>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+
+        <small><i><FiCalendar/></i>15 Mar 2021</small>
+        <small><i><FiUser/></i>Joseph Oliveira</small>
+      </div>
+      <div className={styles.content}>
+        <h1>Como utilizar Hooks</h1>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+
+        <small><i><FiCalendar/></i>15 Mar 2021</small>
+        <small><i><FiUser/></i>Joseph Oliveira</small>
+      </div>
+      <div className={styles.content}>
+        <h1>Como utilizar Hooks</h1>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+
+        <small><i><FiCalendar/></i>15 Mar 2021</small>
+        <small><i><FiUser/></i>Joseph Oliveira</small>
+      </div>
+
+      <div className={styles.content}>
+        <h1>Como utilizar Hooks</h1>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+
+        <small><i><FiCalendar/></i>15 Mar 2021</small>
+        <small><i><FiUser/></i>Joseph Oliveira</small>
+      </div>
+
+
+      <button>Carregar mais posts</button>
+    </div>
   )
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+// export const getStaticProps: GetStaticProps = async () => {
+//   const prismic = getPrismicClient({});
+//   const postsResponse = await prismic.getByType('posts');
 
-//   // TODO
+//   return {
+//     props: {
+//       postsResponse,
+//     },
+//     revalidate: 60 * 60 * 24 // 1 Day
+    
+//   }
 // };
