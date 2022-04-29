a10469071fab1d171abf529d9b425a1d0449d371
Pietro Rhyan <pietroqjg@gmail.com>
2022-04-29 18:29:55 -0300

    Adding home funcionalities

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,10 @@ import { FiCalendar, FiUser } from 'react-icons/fi'
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { useState } from 'react';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import Link from 'next/link';
 
 interface Post {
   uid?: string;
@@ -25,54 +29,103 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ postsPagination }: HomeProps) {
+  const formattedPost = postsPagination.results.map( post => {
+    return {
+      ...post,
+      first_publication_date: format( 
+        new Date(post.first_publication_date), 
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      )
+    }
+  })
+
+  const [posts, setPosts] = useState<Post[]>(formattedPost)
+  const [nextPage, setNextPage] = useState(postsPagination.next_page)
+  const [currentPage, setCurrentPage] = useState(1)
+
+  async function handleNewPosts() {
+    if(currentPage !== 1 && nextPage === null) {
+      return
+    }
+
+    const morePosts = await fetch(`${nextPage}`)
+      .then( response => response.json())
+
+    setNextPage(morePosts.next_page)
+    setCurrentPage(morePosts.page)
+
+    const newPosts = morePosts.results.map( post => {
+      return {
+        uid: post.uid,
+        first_publication_date: format(
+          new Date(post.first_publication_date), 
+          'dd MMM yyyy',
+          {
+            locale: ptBR,
+          }
+        ),
+        data: {
+          author: post.data.author,
+          title: post.data.title,
+          subtitle: post.data.subtitle
+        } 
+      }
+    })
+
+    setPosts([...posts, ...newPosts])
+  }
+
+
   return (
     <div className={styles.container}>
-      <div className={styles.content}>
-        <h1>Como utilizar Hooks</h1>
-        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+      
+      {posts.map( post => (
+        <Link href={`/posts/${post.uid}`} key={post.uid}>
+          <div className={styles.content}>
+            <h1>{post.data.title}</h1>
+            <p>{post.data.subtitle}</p>
 
-        <small><i><FiCalendar/></i>15 Mar 2021</small>
-        <small><i><FiUser/></i>Joseph Oliveira</small>
-      </div>
-      <div className={styles.content}>
-        <h1>Como utilizar Hooks</h1>
-        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <small><i><FiCalendar/></i>{post.first_publication_date}</small>
+            <small><i><FiUser/></i>{post.data.author}</small>
+          </div>
+        </Link>
+      ))}
 
-        <small><i><FiCalendar/></i>15 Mar 2021</small>
-        <small><i><FiUser/></i>Joseph Oliveira</small>
-      </div>
-      <div className={styles.content}>
-        <h1>Como utilizar Hooks</h1>
-        <p>Pensando em sincronização em vez de ciclos de vida.</p>
-
-        <small><i><FiCalendar/></i>15 Mar 2021</small>
-        <small><i><FiUser/></i>Joseph Oliveira</small>
-      </div>
-
-      <div className={styles.content}>
-        <h1>Como utilizar Hooks</h1>
-        <p>Pensando em sincronização em vez de ciclos de vida.</p>
-
-        <small><i><FiCalendar/></i>15 Mar 2021</small>
-        <small><i><FiUser/></i>Joseph Oliveira</small>
-      </div>
-
-
-      <button>Carregar mais posts</button>
+      <button onClick={handleNewPosts}>Carregar mais posts</button>
     </div>
   )
 }
 
-// export const getStaticProps: GetStaticProps = async () => {
-//   const prismic = getPrismicClient({});
-//   const postsResponse = await prismic.getByType('posts');
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('posts', {
+    pageSize: 1,
+  });
 
-//   return {
-//     props: {
-//       postsResponse,
-//     },
-//     revalidate: 60 * 60 * 24 // 1 Day
-    
-//   }
-// };
+  const posts = postsResponse.results.map( post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        author: post.data.author,
+        title: post.data.title,
+        subtitle: post.data.subtitle
+      } 
+    }
+  })
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  }
+ 
+  return {
+    props: {
+      postsPagination,
+    }
+  }
+};
